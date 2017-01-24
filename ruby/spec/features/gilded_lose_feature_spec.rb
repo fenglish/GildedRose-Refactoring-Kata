2a3c6c059a8241a0d0320ed38986c34e98a0594a
Asuka Ochi <asukaochi@gmail.com>
2017-01-24 20:08:18 +0000

    add a feature test about degrading item's quality twice as fast after sell_in has passed

---
+++ ruby/spec/features/gilded_lose_feature_spec.rb
@@ -0,0 +1,19 @@
+require 'item'
+require 'gilded_rose'
+
+describe GildedRose do
+  # Once the sell by date has passed, Quality degrades twice as fast
+  it "should degrades item's quality twice as fast, once the sell by date has passed" do
+    items = [ Item.new("Normal item", 0, 5) ]
+    GildedRose.new( items ).update_quality()
+    expect( items[0].quality ).to eq 3
+  end
+  # The Quality of an item is never negative
+  # “Aged Brie” actually increases in Quality the older it gets
+  # The Quality of an item is never more than 50
+  # “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
+  # “Backstage passes”, like aged brie, increases in Quality as it’s SellIn value approaches; Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
+  #
+  # We have recently signed a supplier of conjured items. This requires an update to our system:
+  # “Conjured” items degrade in Quality twice as fast as normal items
+end
