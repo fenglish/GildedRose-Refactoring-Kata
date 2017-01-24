9cb95653e2e1e4bd953fdc72c8a2596212735d1c
Asuka Ochi <asukaochi@gmail.com>
2017-01-24 20:21:31 +0000

    add a feature test about increasing item's quality never more than 50

--- ruby/spec/features/gilded_lose_feature_spec.rb
+++ ruby/spec/features/gilded_lose_feature_spec.rb
@@ -10,7 +10,7 @@ describe GildedRose do
   end
 
   # The Quality of an item is never negative
-  it "should not degrades item's quality more than 0" do
+  it "should not degrade item's quality more than 0" do
     items = [ Item.new("Normal item", 5, 0) ]
     GildedRose.new( items ).update_quality()
     expect( items[0].quality ).to eq 0
@@ -24,6 +24,12 @@ describe GildedRose do
   end
 
   # The Quality of an item is never more than 50
+  it "should not increase the quality more than 50" do
+    items = [ Item.new("Aged Brie", 5, 50) ]
+    GildedRose.new( items ).update_quality()
+    expect( items[0].quality ).to eq 50
+  end
+
   # “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
   # “Backstage passes”, like aged brie, increases in Quality as it’s SellIn value approaches; Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
   #
