868fc152fd10442f3cf29624fbf9e9ab3c6ad6bb
Asuka Ochi <asukaochi@gmail.com>
2017-01-24 20:29:26 +0000

    add a feature test about never changing Sulfuras' quality and sell_in

--- ruby/spec/features/gilded_lose_feature_spec.rb
+++ ruby/spec/features/gilded_lose_feature_spec.rb
@@ -1,7 +1,7 @@
 require 'item'
 require 'gilded_rose'
 
-describe GildedRose do
+describe GildedRose, "features" do
   # Once the sell by date has passed, Quality degrades twice as fast
   it "should degrades item's quality twice as fast, once the sell by date has passed" do
     items = [ Item.new("Normal item", 0, 5) ]
@@ -31,6 +31,13 @@ describe GildedRose do
   end
 
   # “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
+  it "should not change Sulfuras' quality and sell_in" do
+    items = [ Item.new("Sulfuras, Hand of Ragnaros", 5, 5) ]
+    GildedRose.new( items ).update_quality()
+    expect( items[0].sell_in ).to eq 5
+    expect( items[0].quality ).to eq 5
+  end
+
   # “Backstage passes”, like aged brie, increases in Quality as it’s SellIn value approaches; Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
   #
   # We have recently signed a supplier of conjured items. This requires an update to our system:
