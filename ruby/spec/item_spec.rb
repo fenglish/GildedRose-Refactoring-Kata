867a5614bd0912b0295b1650e76e658d0545fa49
Asuka Ochi <asukaochi@gmail.com>
2017-01-24 17:58:36 +0000

    Pass tests: Item class should  pass all arguments to each method

--- ruby/spec/item_spec.rb
+++ ruby/spec/item_spec.rb
@@ -7,6 +7,18 @@ describe Item do
   let( :sell_in ){ double :sell_in }
   let( :quality ){ double :quality }
 
+  it "should pass the argument name to name" do
+    expect( item.name ).to eq name
+  end
+
+  it "should pass the argument sell_in to sell_in" do
+    expect( item.sell_in ).to eq sell_in
+  end
+
+  it "should pass the argument quality to quality" do
+    expect( item.quality ).to eq quality
+  end
+
   context "#to_s" do
     it "should display name, sell_in, quality" do
       expect( item.to_s ).to eq "#{name}, #{sell_in}, #{quality}"
