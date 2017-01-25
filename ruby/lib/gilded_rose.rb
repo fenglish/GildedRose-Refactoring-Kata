42ff1195576831df10396480122488077947b0c2
Asuka Ochi <asukaochi@gmail.com>
2017-01-25 10:31:07 +0000

    pass each item object to instance variable @item

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -2,59 +2,61 @@ class GildedRose
 
   def initialize(items)
     @items = items
+    @item = nil
   end
 
   def update_quality()
     @items.each do |item|
-      take_different_process_by_item_name(item)
-      decrease_sell_in(item)
+      @item = item
+      take_different_process_by_name
+      decrease_sell_in
     end
   end
 
   private
 
-  def take_different_process_by_item_name(item)
-    case item.name
-    when "Backstage passes to a TAFKAL80ETC concert" then process_for_backstage_passes(item)
+  def take_different_process_by_name
+    case @item.name
+    when "Backstage passes to a TAFKAL80ETC concert" then process_for_backstage_passes
     when "Sulfuras, Hand of Ragnaros"                then process_for_sulfuras
-    when "Aged Brie"                                 then process_for_aged_brie(item)
-    else process_for_normal_item(item) end
+    when "Aged Brie"                                 then process_for_aged_brie
+    else process_for_normal_item end
   end
 
-  def decrease_sell_in(item)
-    item.sell_in -= 1 if item.name != "Sulfuras, Hand of Ragnaros"
+  def decrease_sell_in
+    @item.sell_in -= 1 if @item.name != "Sulfuras, Hand of Ragnaros"
   end
 
-  def process_for_backstage_passes(item)
-    if    item.sell_in <= 0  then item.quality = 0
-    elsif item.sell_in <= 5  then increase_quality( 3, item )
-    elsif item.sell_in <= 10 then increase_quality( 2, item )
-    else  degrade_quality(1,item) end
+  def process_for_backstage_passes
+    if    @item.sell_in <= 0  then @item.quality = 0
+    elsif @item.sell_in <= 5  then increase_quality( 3 )
+    elsif @item.sell_in <= 10 then increase_quality( 2 )
+    else  degrade_quality( 1 ) end
   end
 
   def process_for_sulfuras
     # “Sulfuras”, being a legendary item, never has to decrease in Quality
   end
 
-  def process_for_aged_brie(item)
-    increase_quality( 1, item )
+  def process_for_aged_brie
+    increase_quality( 1 )
   end
 
-  def process_for_normal_item(item)
-    return degrade_quality(2,item) if item.sell_in <= 0
-    degrade_quality(1,item)
+  def process_for_normal_item
+    return degrade_quality( 2 ) if @item.sell_in <= 0
+    degrade_quality( 1 )
   end
 
-  def quality_under_50?(item)
-    item.quality < 50
+  def quality_under_50?
+    @item.quality < 50
   end
 
-  def increase_quality(number,item)
-    item.quality += number if quality_under_50?(item)
+  def increase_quality( number )
+    @item.quality += number if quality_under_50?
   end
 
-  def degrade_quality(number,item)
-    item.quality -= number if item.quality > 0
+  def degrade_quality( number )
+    @item.quality -= number if @item.quality > 0
   end
 
   # def update_quality()
