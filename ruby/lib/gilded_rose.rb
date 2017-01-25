0eac9bac68595690966ccf19e863524239abc916
Asuka Ochi <asukaochi@gmail.com>
2017-01-25 08:56:19 +0000

    extract methods for each process

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -8,22 +8,13 @@ class GildedRose
     @items.each do |item|
       case item.name
       when "Backstage passes to a TAFKAL80ETC concert" then
-        if item.sell_in == 0
-          item.quality = 0
-        elsif item.sell_in <= 5
-          item.quality += 3 if quality_over_50?(item)
-        elsif item.sell_in <= 10
-          item.quality += 2 if quality_over_50?(item)
-        else
-          item.quality -= 1
-        end
+        process_for_backstage_passes(item)
       when "Sulfuras, Hand of Ragnaros" then
-        # “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
+        # “Sulfuras”, being a legendary item, never has to decrease in Quality
       when "Aged Brie" then
-        item.quality += 1 if quality_over_50?(item)
+        process_for_aged_brie(item)
       else
-        item.quality -= 1 if item.quality > 0 && item.sell_in > 0
-        item.quality -= 2 if item.quality > 0 && item.sell_in == 0
+        process_for_normal_item(item)
       end
       decrease_sell_in(item)
     end
@@ -37,6 +28,27 @@ class GildedRose
 
   def quality_over_50?(item)
     item.quality < 50
+  end
+
+  def process_for_backstage_passes(item)
+    if item.sell_in == 0 then
+      item.quality = 0
+    elsif item.sell_in <= 5
+      item.quality += 3 if quality_over_50?(item)
+    elsif item.sell_in <= 10
+      item.quality += 2 if quality_over_50?(item)
+    else
+      item.quality -= 1
+    end
+  end
+
+  def process_for_aged_brie(item)
+    item.quality += 1 if quality_over_50?(item)
+  end
+
+  def process_for_normal_item(item)
+    item.quality -= 1 if item.quality > 0 && item.sell_in > 0
+    item.quality -= 2 if item.quality > 0 && item.sell_in == 0
   end
 
   # def update_quality()
